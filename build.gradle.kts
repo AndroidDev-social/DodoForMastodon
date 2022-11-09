be250fa984b30fdd25a79fc7cb31431c089007ab
Roshan Rai <rrai@lb.com>
2022-11-09 16:54:46 +0530

    Gradle AGP update

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.20")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
     }
