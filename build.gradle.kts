2e27391b4259c224e3f52bfb553e49b3cb7ddb86
racka98 <bmkilaha@gmail.com>
2022-11-09 20:27:20 +0300

    configure detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
+plugins {
+    // Suppressed because of https://youtrack.jetbrains.com/issue/KTIJ-19369
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.io.gitlab.arturbosch.detekt.gradle)
+}
+
 buildscript {
 
     extra["targetSDKVersion"] = 33
@@ -27,16 +36,29 @@ allprojects {
     }
 }
 
-// Workaround for:
-//
-// The Kotlin source set androidAndroidTestRelease was configured but not added to any
-// Kotlin compilation. You can add a source set to a target's compilation by connecting it
-// with the compilation's default source set using 'dependsOn'.
-// See https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#connecting-source-sets
-//
-// Remove log pollution until Android support in KMP improves.
-//
 subprojects {
+    /**
+     * Start Configuring Detekt
+     */
+    coreDetektSetup()
+
+    beforeEvaluate {
+        dependencies {
+            detektPlugins(libs.io.gitlab.arturbosch.detekt.formatting)
+            detektPlugins(libs.com.twitter.compose.rules.detekt)
+        }
+        filterDetektFromCheckTask()
+    }
+
+    /**
+     * Workaround for:
+     * The Kotlin source set androidAndroidTestRelease was configured but not added to any
+     * Kotlin compilation. You can add a source set to a target's compilation by connecting it
+     * with the compilation's default source set using 'dependsOn'.
+     * See https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#connecting-source-sets
+     *
+     * Remove log pollution until Android support in KMP improves.
+     */
     afterEvaluate {
         project.extensions.findByType<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension>()?.let { ext ->
             ext.sourceSets.removeAll { sourceSet ->
@@ -47,6 +69,74 @@ subprojects {
                     "androidTestFixturesRelease",
                 ).contains(sourceSet.name)
             }
+        }
+    }
+}
+
+/**
+ * To run detekt simply ./gradlew module:detekt
+ */
+fun Project.coreDetektSetup() {
+    // Apply Plugin to sub-project
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    // Configure Detekt
+    detekt {
+        config = files("$rootDir/config/detekt/detekt.yml")
+        buildUponDefaultConfig = true
+        ignoredBuildTypes = listOf("release")
+        source = files(
+            io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_SRC_DIR_JAVA,
+            io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_TEST_SRC_DIR_JAVA,
+            io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_SRC_DIR_KOTLIN,
+            io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_TEST_SRC_DIR_KOTLIN,
+            // Kotlin Multiplatform
+            "src/commonMain/kotlin",
+            "src/androidMain/kotlin",
+            "src/iosMain/kotlin",
+            "src/jvmMain/kotlin",
+            "src/desktopMain/kotlin",
+            "src/jsMain/kotlin",
+        )
+    }
+
+    tasks.withType<Detekt>().configureEach detekt@{
+        exclude("**/build/**", "**/generated/**", "**/resources/**")
+        basePath = rootProject.projectDir.absolutePath
+        autoCorrect = true // Auto corrects common formatting issues
+        // Configure reports here
+        reports {
+            xml.required.set(false)
+            html.required.set(true)
+            txt.required.set(false)
+            sarif.required.set(false)
+            md.required.set(false)
+
+            html {
+                required.set(true)
+                outputLocation.set(
+                    layout.buildDirectory.file("reports/detekt.html")
+                )
+            }
+        }
+    }
+
+    tasks.withType<DetektCreateBaselineTask>().configureEach detekt@{
+        exclude("**/build/**", "**/generated/**", "**/resources/**")
+        basePath = rootProject.projectDir.absolutePath
+    }
+}
+
+/**
+ * By default Detekt runs when the Gradle :check task is triggered
+ * It should be disabled for now until everything has been correctly setup (including CI)
+ */
+fun Project.filterDetektFromCheckTask() {
+    if (tasks.names.contains("check")) {
+        tasks.named("check").configure {
+            this.setDependsOn(this.dependsOn.filterNot {
+                it is TaskProvider<*> && it.name == "detekt"
+            })
         }
     }
 }
