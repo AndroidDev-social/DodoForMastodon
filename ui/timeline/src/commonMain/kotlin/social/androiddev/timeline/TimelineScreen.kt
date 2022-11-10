b148f824532a68f9880dbfd965335963e24640c9
crocsandcoffee <o.ghenatnevi@gmail.com>
2022-11-10 00:34:26 -0500

    comment out preview

--- ui/timeline/src/commonMain/kotlin/social/androiddev/timeline/TimelineScreen.kt
+++ ui/timeline/src/commonMain/kotlin/social/androiddev/timeline/TimelineScreen.kt
@@ -1,29 +1,17 @@
 package social.androiddev.timeline
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import social.androiddev.common.composables.UserAvatar
 import social.androiddev.common.theme.MastodonTheme
-import social.androiddev.domain.timeline.model.Account
-import social.androiddev.domain.timeline.model.Status
 
 @Composable
 fun TimelineScreen(
@@ -49,7 +37,7 @@ fun TimelineScreen(
 
 // skip preview to work with multiplatform
 // https://github.com/JetBrains/compose-jb/issues/1603
-@Preview
+//@Preview
 @Composable
 private fun TimelinePreview() {
     MastodonTheme {
