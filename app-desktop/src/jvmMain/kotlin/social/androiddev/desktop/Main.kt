db8496504b7646fa8e0f71f1696f99736397fc34
crocsandcoffee <o.ghenatnevi@gmail.com>
2022-11-12 13:38:39 -0500

    remove imports

--- app-desktop/src/jvmMain/kotlin/social/androiddev/desktop/Main.kt
+++ app-desktop/src/jvmMain/kotlin/social/androiddev/desktop/Main.kt
@@ -1,24 +1,12 @@
 package social.androiddev.desktop
 
 import WelcomeScreen
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import social.androiddev.common.theme.MastodonTheme
-import social.androiddev.common.utils.AsyncImage
-import social.androiddev.common.utils.loadImageIntoPainter
-import social.androiddev.timeline.TimelineScreen
-import social.androiddev.timeline.dummyFeedItem
 
 fun main() {
     application {
