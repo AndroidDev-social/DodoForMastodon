02e171c0b38db3700d1511b21264f1765c461a39
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2022-11-13 06:55:14 +0100

    adjust the urlstream contract to work on native as well

--- ui/common/src/androidMain/kotlin/social/androiddev/common/utils/ImageLoading.kt
+++ ui/common/src/androidMain/kotlin/social/androiddev/common/utils/ImageLoading.kt
@@ -5,12 +5,10 @@ import android.graphics.BitmapFactory
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
-import java.io.ByteArrayInputStream
 import social.androiddev.common.network.urlStream
 
 actual suspend fun loadImageIntoPainter(url: String): Painter {
-    val inputStream: ByteArrayInputStream = urlStream(url)
-    val byteArray: ByteArray = inputStream.readBytes()
+    val byteArray: ByteArray = urlStream(url)
     val bitmap: Bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
 
     return BitmapPainter(bitmap.asImageBitmap())
