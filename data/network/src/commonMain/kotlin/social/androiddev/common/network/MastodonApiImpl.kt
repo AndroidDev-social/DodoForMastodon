bfc1c6f467c096e084ca32d1158c3481ca1cf923
crocsandcoffee <o.ghenatnevi@gmail.com>
2022-11-16 16:12:44 -0500

    formatting

--- data/network/src/commonMain/kotlin/social/androiddev/common/network/MastodonApiImpl.kt
+++ data/network/src/commonMain/kotlin/social/androiddev/common/network/MastodonApiImpl.kt
@@ -22,8 +22,7 @@ import social.androiddev.common.network.util.runCatchingIgnoreCancelled
 
 class MastodonApiImpl(
     private val httpClient: HttpClient,
-
-    ) : MastodonApi {
+) : MastodonApi {
     override suspend fun getInstance(domain: String?): Result<Instance> {
         return try {
             Result.success(
