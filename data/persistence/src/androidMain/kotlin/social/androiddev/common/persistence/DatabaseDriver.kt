90a899f59cd62e589bdeefc3d76ce8f5b8f796d0
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2022-11-18 07:02:58 +0100

    Setup SQLDelight (#70)

---
+++ data/persistence/src/androidMain/kotlin/social/androiddev/common/persistence/DatabaseDriver.kt
@@ -0,0 +1,20 @@
+/*
+ * This file is part of MastodonX.
+ *
+ * MastodonX is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * MastodonX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MastodonX. If not, see <https://www.gnu.org/licenses/>.
+ */
+package social.androiddev.common.persistence
+
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+
+actual suspend fun provideDatabaseDriver(schema: SqlDriver.Schema): SqlDriver {
+    // TODO: replace with AndroidSqliteDriver and inject context
+    return JdbcSqliteDriver(url = JdbcSqliteDriver.IN_MEMORY).also { driver ->
+        schema.create(driver = driver)
+    }
+}
